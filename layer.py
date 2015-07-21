83310bc4e01838b8540c57d62262c3b7d0791e61
Sohil Patel <sohilpatel4932@gmail.com>
2015-07-22 01:52:45 +0530

    Test

--- layer.py
+++ layer.py
@@ -2,18 +2,6 @@ from yowsup.layers.interface import YowInterfaceLayer, ProtocolEntityCallback
 
 class EchoLayer(YowInterfaceLayer):
 
-    @ProtocolEntityCallback("message")
-    def onMessage(self, messageProtocolEntity):
-
-        if messageProtocolEntity.getType() == 'text':
-            self.onTextMessage(messageProtocolEntity)
-        elif messageProtocolEntity.getType() == 'media':
-            self.onMediaMessage(messageProtocolEntity)
-
-        self.toLower(messageProtocolEntity.forward(messageProtocolEntity.getFrom()))
-        self.toLower(messageProtocolEntity.ack())
-        self.toLower(messageProtocolEntity.ack(True))
-
 
     @ProtocolEntityCallback("receipt")
     def onReceipt(self, entity):

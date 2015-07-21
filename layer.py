63de0b331310a3a71eba040d57b291bdb396f4a5
Sohil Patel <sohilpatel4932@gmail.com>
2015-07-22 01:50:02 +0530

    Test

--- layer.py
+++ layer.py
@@ -1,74 +1,35 @@
-from yowsup.layers.interface                           import YowInterfaceLayer, ProtocolEntityCallback
-from yowsup.layers.protocol_messages.protocolentities  import TextMessageProtocolEntity
-from yowsup.layers.protocol_receipts.protocolentities  import OutgoingReceiptProtocolEntity
-from yowsup.layers.protocol_acks.protocolentities      import OutgoingAckProtocolEntity
-
-import time
-import datetime
-import json
-import unirest
-
+from yowsup.layers.interface import YowInterfaceLayer, ProtocolEntityCallback
 
 class EchoLayer(YowInterfaceLayer):
 
     @ProtocolEntityCallback("message")
     def onMessage(self, messageProtocolEntity):
-        #send receipt otherwise we keep receiving the same message over and over
 
-        if True:
-            receipt = OutgoingReceiptProtocolEntity(messageProtocolEntity.getId(), messageProtocolEntity.getFrom())
+        if messageProtocolEntity.getType() == 'text':
+            self.onTextMessage(messageProtocolEntity)
+        elif messageProtocolEntity.getType() == 'media':
+            self.onMediaMessage(messageProtocolEntity)
 
-        print messageProtocolEntity.getFrom();
+        self.toLower(messageProtocolEntity.forward(messageProtocolEntity.getFrom()))
+        self.toLower(messageProtocolEntity.ack())
+        self.toLower(messageProtocolEntity.ack(True))
 
-        wordformean = messageProtocolEntity.getBody().lower()
-        response = self.dictionaryword(wordformean)
-
-        phone =  messageProtocolEntity.getFrom()
-
-        if '@' in phone:
-            outgoingMessageProtocolEntity = TextMessageProtocolEntity(message, to = phone)
-        elif '-' in phone:
-            outgoingMessageProtocolEntity = TextMessageProtocolEntity(message, to = "%s@g.us" % phone)
-        else:
-            outgoingMessageProtocolEntity = TextMessageProtocolEntity(message, to = "%s@s.whatsapp.net" % phone)
-
-        self.toLower(receipt)
-        self.toLower(outgoingMessageProtocolEntity)
 
     @ProtocolEntityCallback("receipt")
     def onReceipt(self, entity):
-        ack = OutgoingAckProtocolEntity(entity.getId(), "receipt", "delivery", entity.getFrom())
-        self.toLower(ack)
+        self.toLower(entity.ack())
 
-    def dictionaryword(self, word):
+    def onTextMessage(self,messageProtocolEntity):
+        # just print info
+        print("Echoing %s to %s" % (messageProtocolEntity.getBody(), messageProtocolEntity.getFrom(False)))
 
-        url = "https://montanaflynn-dictionary.p.mashape.com/define?word=" + word
+    def onMediaMessage(self, messageProtocolEntity):
+        # just print info
+        if messageProtocolEntity.getMediaType() == "image":
+            print("Echoing image %s to %s" % (messageProtocolEntity.url, messageProtocolEntity.getFrom(False)))
 
-        # These code snippets use an open-source library.
-        response = unirest.get(url,
-            headers={
-                "X-Mashape-Key": "j6rDcjfVcVmshxp0Y102O2cL6vDrp16mL1FjsnsgRqpcl6fC3L",
-                "Accept": "application/json"
-            }
-        )
-        
-        
-        resp = word + '\n\n'
-        
-        if ' ' in word:
-                text_response = "Dictionary word does not allow 'Space'"
-        else:
+        elif messageProtocolEntity.getMediaType() == "location":
+            print("Echoing location (%s, %s) to %s" % (messageProtocolEntity.getLatitude(), messageProtocolEntity.getLongitude(), messageProtocolEntity.getFrom(False)))
 
-            data = json.dumps(response.body, separators=(',',':'))
-            meanings = (json.loads(data))["definitions"]
-        
-            count = 0
-            for meaning in meanings:
-                count = count + 1
-                resp = resp + 'm' + str(count) +' : ' + str(meaning["text"].encode('ascii', 'ignore')) + '\n\n'
-
-            # Create Text response
-            text_response = str(resp)
-
-        #Return details
-        return str(text_response)
+        elif messageProtocolEntity.getMediaType() == "vcard":
+            print("Echoing vcard (%s, %s) to %s" % (messageProtocolEntity.getName(), messageProtocolEntity.getCardData(), messageProtocolEntity.getFrom(False)))
